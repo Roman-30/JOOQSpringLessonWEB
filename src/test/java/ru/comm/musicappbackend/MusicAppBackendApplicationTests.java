7e8f94d24018027ba5bb3ac3b927acefb0016809
Roman-30 <Romses2016@yandex.ru>
2024-10-05 19:02:36 +0300

    init project

---
+++ src/test/java/ru/comm/musicappbackend/MusicAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.comm.musicappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
